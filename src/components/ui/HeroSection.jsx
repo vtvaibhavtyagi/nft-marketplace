ee9be513968ea73d15f50281b368f331b7965098
Vaibhav Tyagi <binarycoder@Vaibhavs-MacBook-Pro.local>
2022-11-05 14:57:12 +0530

    first commit

---
+++ src/components/ui/HeroSection.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./hero-section.css";
+
+import heroImg from "../../assets/images/hero.jpg";
+
+const HeroSection = () => {
+  return (
+    <section className="hero__section">
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="hero__content">
+              <h2>
+                Create and Mortgage your NFTs 
+                <span>Invest</span> in rare digital art 
+              </h2>
+              <p>
+                Nomura Brings you a Platform where you can Create your NFTs, Mortgage it and Invest in Others NFTs
+              </p>
+
+              <div className="hero__btns d-flex align-items-center gap-4">
+                <button className=" explore__btn d-flex align-items-center gap-2">
+                  <i class="ri-rocket-line"></i>{" "}
+                  <Link to="/market">Explore</Link>
+                </button>
+                <button className=" create__btn d-flex align-items-center gap-2">
+                  <i class="ri-ball-pen-line"></i>
+                  <Link to="/create">Create</Link>
+                </button>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="hero__img">
+              <img src={heroImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
